282cb22c3948c9d23824c393d270156c66e8877b
samplawski <tsamplawski@gmail.com>
2023-06-14 14:23:50 +0200

    Updates

---
+++ precache-manifest.2ddcf9393ba0f5b66895c98dfd5f326b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c88feefa781bb709fb",
+    "url": "/todo-list-react/static/css/main.4843d6d2.chunk.css"
+  },
+  {
+    "revision": "74c88feefa781bb709fb",
+    "url": "/todo-list-react/static/js/main.74c88fee.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/todo-list-react/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/todo-list-react/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "36f7c921d3c052ae6828",
+    "url": "/todo-list-react/static/js/runtime~main.36f7c921.js"
+  },
+  {
+    "revision": "85a34d10ed349dfd22da5b9f67547255",
+    "url": "/todo-list-react/index.html"
+  }
+];

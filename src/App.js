2e841180025d8eaf75769a448bc78387cee34601
samplawski <tsamplawski@gmail.com>
2023-06-15 05:55:27 +0200

    import React from.., openssl-legacy-provider added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Container from "./Container"
 import Header from "./Header"
 import Section from "./Section";
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Header = ({ title }) => (
     <header>
         <h1>{title}</h1>

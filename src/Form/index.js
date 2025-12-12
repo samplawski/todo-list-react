e34450a9412fdcaf2261d1de478ddd276ada8882
samplawski <tsamplawski@gmail.com>
2025-12-12 10:54:06 +0100

    feat: focus on new task field added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    focusInput();
+  }, []);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
 
     const trimmedContent = newTaskContent.trim();
     if (trimmedContent.length === 0) {
+      inputRef.current.focus();
       return;
     }
     addNewTask(trimmedContent);
     setNewTaskContent("");
+    focusInput();
   };
 
   return (
     <form className="form" onSubmit={onFormSubmit}>
       <input
+        ref={inputRef}
         value={newTaskContent}
         className="form__newTask"
         placeholder="Co jest do zrobienia?"

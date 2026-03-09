7ed37b51621891926a8a62599cc871511730f3f2
samplawski <tsamplawski@gmail.com>
2026-03-09 16:23:59 +0100

    fix: resolve path issue for exampleTasks.json using PUBLIC_URL

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,6 +1,6 @@
 export const getExampleTasks = async () => {
-  const response = await fetch("/todo-list-react/exampleTasks.json");
-  // const response = await fetch("exampleTasks.json");
+  // const response = await fetch("/todo-list-react/exampleTasks.json");
+  const response = await fetch(`${process.env.PUBLIC_URL}/exampleTasks.json`);
 
   if (!response.ok) {
     throw new Error(response.statusText);

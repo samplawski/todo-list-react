7095d57983451dd1142224d6594b733d720b983e
samplawski <tsamplawski@gmail.com>
2026-03-24 22:10:01 +0100

    feat: add custom hooks for query parameters and refactor Search component - created useQueryParameter and useReplaceQueryParameter hooks - refactored Search component to use new hooks - switched from history.push to history.replace for better UX during searching

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,23 +1,17 @@
-import { useHistory, useLocation } from "react-router-dom";
 import Input from "../../Input";
 import { Wrapper } from "./styled";
 import searchQueryParamName from "../searchQueryParamName";
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
 
 export default () => {
-  const location = useLocation();
-  const history = useHistory();
-  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
   const onInputChange = ({ target }) => {
-    const searchPatams = new URLSearchParams(location.search);
-
-    if (target.value.trim() === "") {
-      searchPatams.delete(searchQueryParamName);
-    } else {
-      searchPatams.set(searchQueryParamName, target.value);
-    }
-
-    history.push(`${location.pathname}?${searchPatams.toString()}`);
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
   };
 
   return (
---
+++ src/features/tasks/TasksPage/Search/queryParameters.js
@@ -0,0 +1,22 @@
+import { useHistory, useLocation } from "react-router-dom";
+
+export const useQueryParameter = (key) => {
+  const location = useLocation();
+  return new URLSearchParams(location.search).get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  return ({ key, value }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (value === undefined || value.trim() === "") {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+    history.replace(`${location.pathname}?${searchParams.toString()}`);
+  };
+};
